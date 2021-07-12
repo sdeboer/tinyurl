54d756644e98ab575732c217cce8e141d3ed765a
Simon de Boer <sdeboer@ingamer.com>
2021-07-12 17:19:52 +0000

    Use some scaffolding to get the CRUD stuff done.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,10 +1,6 @@
 class LinksController < ApplicationController
-  def index
-  end
-
-  def edit
-  end
-
+  before_action :set_link, only: %i[show edit update destroy]
+  
   def destination
     @link = Link.find_by_id params[:token]
     if @link.nil?
@@ -15,10 +11,64 @@ class LinksController < ApplicationController
     end
   end
 
+  def index
+    @links = Link.all
+  end
+
+  def edit
+  end
+
   def show
-    @link = Link.find param[:token]
   end
 
   def new
+    @link = Link.new
+  end
+  
+  def create
+    @link = Link.new(link_params)
+
+    respond_to do |format|
+      if @link.save
+        format.html { redirect_to @link, notice: "Link was successfully created." }
+        format.json { render :show, status: :created, location: @link }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /links/1 or /links/1.json
+  def update
+    respond_to do |format|
+      if @link.update(link_params)
+        format.html { redirect_to @link, notice: "Link was successfully updated." }
+        format.json { render :show, status: :ok, location: @link }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /links/1 or /links/1.json
+  def destroy
+    @link.destroy
+    respond_to do |format|
+      format.html { redirect_to links_url, notice: "Link was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  
+  private
+  
+  def set_link
+    @link = Link.find params[:id]
+  end
+  
+  def link_params
+    params.require(:link).permit(:destination)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'links/index'
-  get 'links/edit'
-  get 'links/:token', to: 'links#show'
-  get 'links/new'
+  resources :links
   
   get '/:token', to: 'links#destination', as: :tiny
 end
